eadaa3637758c89dd0ca8d7995044aeab38a3f92
nimarhmti <nimarahmati2000@gmail.com>
2023-05-13 18:39:57 +0430

    configuration

---
+++ src/Styles/GlobalStyle.ts
@@ -0,0 +1,25 @@
+// import styled from "styled-components";
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700&family=Wix+Madefor+Display:wght@400;500;600;700&display=swap');
+*{
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+}
+html{
+    font-size: 16px;
+    font-family: 'Roboto', sans-serif;
+    font-family: 'Wix Madefor Display', sans-serif;
+}
+
+ul,li{
+    list-style: none;
+   } 
+
+   a{
+    text-decoration: none;
+   }
+
+`;
